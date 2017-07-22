6074bda237a7e58ccbf613fa9780babed37096f4
胡桃夹子 <wangxinforme@163.com>
2017-07-22 18:41:04 +0800

    更新

--- sc-eureka-provider/src/main/java/com/wangxin/common/datasource/DynamicDataSourceContextHolder.java
+++ sc-eureka-provider/src/main/java/com/wangxin/common/datasource/DynamicDataSourceContextHolder.java
@@ -26,6 +26,7 @@ public class DynamicDataSourceContextHolder {
      * @param dataSourceId
      * @return  
      */
+    @SuppressWarnings("unlikely-arg-type")
     public static boolean containsDataSource(String dataSourceId) {
         if(contextHolder.equals(dataSourceId))
             return true;
