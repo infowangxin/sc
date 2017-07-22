aad4000705c82414d76e1efef315e830557c9995
胡桃夹子 <wangxinforme@163.com>
2017-07-22 22:49:26 +0800

    准备集成springcloud config

--- sc-eureka-provider/src/main/java/com/wangxin/Application.java
+++ sc-eureka-provider/src/main/java/com/wangxin/Application.java
@@ -1,19 +1,20 @@
 package com.wangxin;
 
+import com.wangxin.common.datasource.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.wangxin.common.datasource.DynamicDataSourceRegister;
 
 @Import({ DynamicDataSourceRegister.class })
 @EnableTransactionManagement
 @ServletComponentScan
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableConfigServer
 public class Application {
     
     public static void main(String[] args) {
