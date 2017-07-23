34005a537ad46b1a748b60f202d41a527e088e1c
胡桃夹子 <wangxinforme@163.com>
2017-07-23 14:47:46 +0800

    准备集成springcloud config

--- sc-eureka-provider/src/main/java/com/wangxin/remote/impl/simple/SystemController.java
+++ sc-eureka-provider/src/main/java/com/wangxin/remote/impl/simple/SystemController.java
@@ -3,8 +3,10 @@ package com.wangxin.remote.impl.simple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,29 @@ public class SystemController {
     @Autowired
     private DiscoveryClient client;
 
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    @Value("${from:local}")
+    private String from;
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        String result = "来自于服务端<br/>server_id:"
+                + instance.getServiceId() + "<br/>host:"
+                + instance.getHost() + "<br/>port:"
+                + instance.getPort() + "<br/>"
+                + from;
+        logger.info(result);
+        return result;
+    }
+
+    @GetMapping("/info")
     public String info() {
         ServiceInstance instance = client.getLocalServiceInstance();
-        String result = "来自于服务端<br/>server_id:" + instance.getServiceId() + "<br/>host:" + instance.getHost() + "<br/>port:" + instance.getPort();
+        String result = "来自于服务端<br/>server_id:"
+                + instance.getServiceId() + "<br/>host:"
+                + instance.getHost() + "<br/>port:"
+                + instance.getPort() + "<br/>"
+                + from;
         logger.info(result);
         return result;
     }
