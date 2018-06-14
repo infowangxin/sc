9a91351fe8d9b5f9373618381c3726c4320d24de
胡桃夹子 <wangxinforme@163.com>
2018-06-14 15:45:45 +0800

    更新

--- sc-provider/src/main/java/com/wangxin/Swagger2.java
+++ sc-provider/src/main/java/com/wangxin/Swagger2.java
@@ -13,10 +13,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SuppressWarnings({ "all" })
-@Configuration
-@EnableAutoConfiguration
-@EnableSwagger2
+//@SuppressWarnings({ "all" })
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableSwagger2
 public class Swagger2 {
     
     /*
